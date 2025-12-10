3218b0189a5e95fb3173f7eb936a5dd1d0337588
Amy Johnson <150845564+loonsei@users.noreply.github.com>
2025-12-10 19:01:00 +0000

    Add files via upload
    completed option a, working on b

--- stockControl/src/main.java
+++ stockControl/src/main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class main {
 
@@ -6,7 +7,7 @@ public class main {
 	{
 		while (true) 
 		{
-			System.out.println("VG Company Stock Control: pick an option: ");
+			System.out.println("VG Company Stock Control. pick an option: ");
 			System.out.println("a. Add a new video game. ");
 			System.out.println("b. View the details of a video game. ");
 			System.out.println("c. Edit the details of a video game. ");
@@ -16,16 +17,57 @@ public class main {
 			Scanner scnr = new Scanner(System.in);
 			String menuSelect = scnr.nextLine();
 			
+			// for storing objects in array list
+			ArrayList<vg> vgList = new ArrayList<vg>();
+			
 			switch(menuSelect)
 			{
 			case "a":
 			case "A":
 				// asks for name, title, release date, publisher, developer and platform 
+				System.out.println("Enter the title of the video game you want to add: ");
+				String title = scnr.nextLine();
+				System.out.println("Enter the release year of the video game: ");
+				int releaseYear = scnr.nextInt();
+				scnr.nextLine(); // throwaway nextLine bc of the nextInt
+				System.out.println("Enter the publisher of the video game: ");
+				String publish = scnr.nextLine();
+				System.out.println("Enter the developer of the video game: ");
+				String dev = scnr.nextLine();
+				System.out.println("Enter the platform of the video game: ");
+				String platform = scnr.nextLine();
+				
+				// call constructor
+				vg newGame = new vg(title, releaseYear, publish, dev, platform);
+				System.out.println("You have entered the following: "+newGame.title+", "+newGame.releaseYear+", "+newGame.publish+", "+newGame.dev+", "+newGame.platform+".");
+				
+				// store object in list 
+				// ArrayList<vg> vgList = new ArrayList<vg>();
+				vgList.add(newGame);
+				
+				// prints the objects stored in the array 
+				// System.out.println(vgList.get(0).toString());
 				break;
 	
 			case "b":
 			case "B":
-				// displays details 
+				// asks user the title and release date to identify the game they are wanting to view 
+				System.out.println("What is the title of the game you are wanting to view? ");
+				String findTitle = scnr.nextLine();
+				System.out.println("What is the release year of the game you are wanting to view? ");
+				int findYear = scnr.nextInt();
+				// find value in array list
+				// && vgList.contains(findYear)
+				if (vgList.contains(findTitle))
+				{
+					// print the details of objects that have this these details
+					System.out.println("found it, printing details...");
+				}
+				else 
+				{
+					System.out.println("Not found.");					
+				}
+				// try and find way to loop this case if the user makes error
 				break;
 	
 			case "c":
@@ -47,7 +89,7 @@ public class main {
 				break;
 	
 			default:
-				System.out.println("Invalid input");
+				System.out.println("Invalid input, please try again.");
 			}
 		}
 	}
--- stockControl/src/vg.java
+++ stockControl/src/vg.java
@@ -3,8 +3,24 @@ public class vg {
 	
 	String title;
 	int releaseYear;
-	String publisher;
-	String developer;
+	String publish;
+	String dev;
 	String platform;
+	
+	
+	// constructor
+	vg(String title, int releaseYear, String publish, String dev, String platform)
+	{
+		this.title = title;
+		this.releaseYear = releaseYear;
+		this.publish = publish;
+		this.dev = dev;
+		this.platform = platform;
+	}
+	
+	public String toString() {
+		return title+" "+releaseYear+" "+publish+" "+dev+" "+platform;
+	}
+ 
 
 }
