3287554a6b7daea359042fba8dd9e2b9c0b257eb
Amy Johnson <150845564+loonsei@users.noreply.github.com>
2025-12-29 17:15:32 +0000

    finished the menu just need polish

--- stockControl/src/main.java
+++ stockControl/src/main.java
@@ -66,7 +66,7 @@ public class main {
 					{
 						// print the details of objects that have this these details
 						titleFound = true;
-						System.out.println("Found! Showing details...");
+						System.out.println("Found! Showing details...\n");
 						System.out.println("Title: "+g.title+" \nRelease year: "+g.releaseYear+" \nPublisher: "+g.publish+" \nDeveloper: "+g.dev+" \nPlatform: "+g.platform+" \n");
 					}
 					
@@ -83,7 +83,73 @@ public class main {
 			case "c":
 			case "C":
 				// ask user the title and the release date of the item they want to change 
-				System.out.println("What detail would you like to change? Select from the following: \ntitle\netc.");
+				System.out.println("What is the title of the item you would like to delete?");
+				String findTitleForEdit = scnr.nextLine();
+				System.out.println("What is the release date of the item you would like to delete?");
+				int findYearForEdit = scnr.nextInt();
+				scnr.nextLine(); // throwaway nextLine bc of the nextInt
+				boolean feFound = false;
+				
+				for (vg e : vgList) 
+				{
+					if (e.getTitle().equalsIgnoreCase(findTitleForEdit) && e.getReleaseYear() == findYearForEdit);
+					{
+						feFound = true;
+						System.out.println("Found! Showing details...\n");
+						System.out.println("Title: "+e.title+" \nRelease year: "+e.releaseYear+" \nPublisher: "+e.publish+" \nDeveloper: "+e.dev+" \nPlatform: "+e.platform+" \n");
+						
+						System.out.println("What detail would you like to change? Answer from the following: \n- title\n- ry\n- pub\n- dev\n- plat\n");
+						String userAnswer = scnr.nextLine();
+						switch(userAnswer) 
+						{
+						case "title":
+						case "Title":
+						case "TITLE":
+							System.out.println("What do you want to change the title to? Currently: "+e.title+"");
+							String editTitle = scnr.nextLine();
+							e.setTitle(editTitle);
+							System.out.println("Title changed.\n");
+							
+						/*case "ry":
+						case "Ry":
+						case "RY":
+							System.out.println("What do you want to change the title to? Currently: "+e.releaseYear+"");
+							String editRY = scnr.nextLine();
+							e.setReleaseYear(editRY);
+							System.out.println("Release year changed.\n");*/
+							
+						case "pub":
+						case "Pub":
+						case "PUB":
+							System.out.println("What do you want to change the publisher to? Currently: "+e.publish+"");
+							String editPub = scnr.nextLine();
+							e.setPublish(editPub);
+							System.out.println("Publisher changed.\n");
+						
+						case "dev":
+						case "Dev":
+						case "DEV":
+							System.out.println("What do you want to change the developer to? Currently: "+e.dev+"");
+							String editDev = scnr.nextLine();
+							e.setTitle(editDev);
+							System.out.println("Developer changed.\n");
+							
+						case "plat":
+						case "Plat":
+						case "PLAT":
+							System.out.println("What do you want to change the platform to? Currently: "+e.platform+"");
+							String editPlat = scnr.nextLine();
+							e.setTitle(editPlat);
+							System.out.println("Platform changed.\n");
+						}
+					}	
+				}
+				
+				if (!feFound) 
+				{
+					System.out.println("Couldn't find an item with those details.");
+				}
+				
 				break;
 	
 			case "d":
@@ -101,7 +167,7 @@ public class main {
 					if (d.getTitle().equalsIgnoreCase(findTitleForDeletion) && d.getReleaseYear() == findYearForDeletion)
 					{
 						fdFound = true;
-						System.out.println("Found! Showing details...");
+						System.out.println("Found! Showing details...\n");
 						System.out.println("Title: "+d.title+" \nRelease year: "+d.releaseYear+" \nPublisher: "+d.publish+" \nDeveloper: "+d.dev+" \nPlatform: "+d.platform+" \n");
 						System.out.println("Do you want to delete this item? Answer Y or N.");
 						String userAnswer = scnr.nextLine();
