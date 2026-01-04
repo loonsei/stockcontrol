e978b83ac949df5426d68fc34bc4fdaa99a0d2b7
Amy Johnson <150845564+loonsei@users.noreply.github.com>
2026-01-04 20:01:03 +0000

    finished customer menu

--- stockControl/src/custMenu.java
+++ stockControl/src/custMenu.java
@@ -99,11 +99,126 @@ public class custMenu {
 				
 			case "c":
 			case "C":
-
+				// ask user the ID of the item they want to change 
+				System.out.println("What is the ID of the customer you would like to edit?");
+				int findIDForEdit = scnr.nextInt();
+				scnr.nextLine(); // throwaway nextLine bc of the nextInt
+				boolean feFound = false;
+				
+				for (customer i : custList) 
+				{
+					if (i.getCustID() == findIDForEdit);
+					{
+						feFound = true;
+						System.out.println("Found! Showing details...\n");
+						System.out.println("ID: "+i.custID+" \nName: "+i.name+" \nEmail: "+i.email+" \nPhone: "+i.phone+"");
+						// ask user which detail they want to edit 
+						System.out.println("What detail would you like to change? Answer from the following: \n- ID\n- name\n- email\n- phone\n");
+						String userAnswer = scnr.nextLine();
+						switch(userAnswer) 
+						{
+						case "id":
+						case "Id":
+						case "ID":
+							System.out.println("What do you want to change the ID to? Currently: "+i.custID+"");
+							int editID = scnr.nextInt();
+							scnr.nextLine(); // throwaway
+							// sets ID to new value
+							i.setCustID(editID);
+							System.out.println("ID changed.\n");
+							
+							break;
+							
+						case "name":
+						case "Name":
+						case "NAME":
+							System.out.println("What do you want to change the name to? Currently: "+i.name+"");
+							String editName = scnr.nextLine();
+							i.setName(editName);
+							System.out.println("Name changed.\n");
+							
+							break;
+							
+						case "email":
+						case "Email":
+						case "EMAIL":
+							System.out.println("What do you want to change the email to? Currently: "+i.email+"");
+							String editEmail = scnr.nextLine();
+							i.setEmail(editEmail);
+							System.out.println("Email changed.\n");
+							
+							break;
+						
+						case "phone":
+						case "Phone":
+						case "PHONE":
+							System.out.println("What do you want to change the phone number to? Currently: "+i.phone+"");
+							String editPhone = scnr.nextLine();
+							i.setPhone(editPhone);
+							System.out.println("Phone changed.\n");
+							
+							break;
+							
+						default:
+							System.out.println("Invalid input.");
+						}
+						
+						break;
+					}	
+				}
+				
+				if (!feFound) 
+				{
+					System.out.println("Couldn't find a customer with those details.");
+				}
+				
+				// break out of for loop
 				break;
 				
 			case "d":
 			case "D":
+				// ask for ID and name, delete object 
+				System.out.println("What is the ID of the customer you would like to delete?");
+				int findIDForDeletion = scnr.nextInt();
+				scnr.nextLine(); // throwaway
+				System.out.println("What is the name of the customer you would like to delete?");
+				String findNameForDeletion = scnr.nextLine();
+				boolean fdFound = false;
+				
+				for (customer d : custList) 
+				{
+					if (d.getCustID() == findIDForDeletion && d.getName().equalsIgnoreCase(findNameForDeletion))
+					{
+						fdFound = true;
+						System.out.println("Found! Showing details...\n");
+						System.out.println("ID: "+d.custID+" \nName: "+d.name+" \nEmail: "+d.email+" \nPhone: "+d.phone+"\n");
+						System.out.println("Do you want to delete this item? Answer Y or N.");
+						String userAnswer = scnr.nextLine();
+						if (userAnswer.equalsIgnoreCase("Y")) 
+						{
+							// add to forDeletion array list 
+							forDeletion.add(d);
+							System.out.println("Item removed!");
+						}
+						else if (userAnswer.equalsIgnoreCase("N")) 
+						{
+							System.out.println("Returning to main menu...");
+						}
+						else 
+						{
+							System.out.println("Invalid input.");
+						}
+					}
+				}
+				
+				// delete all objects stored on deletion list 
+				// https://www.w3schools.com/java/ref_arraylist_removeall.asp 
+				custList.removeAll(forDeletion);
+				
+				if (!fdFound) 
+				{
+					System.out.println("Couldn't find a customer with those details.");
+				}
 				
 				break; 
 				
